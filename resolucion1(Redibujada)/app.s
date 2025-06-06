789ff66a39254f4d6e01af0be5205aed736de273
MaxGreiff <maximogreiff@gmail.com>
2025-06-05 21:55:12 -0300

    Update app.s

--- resolucion1(Redibujada)/app.s
+++ resolucion1(Redibujada)/app.s
@@ -465,17 +465,17 @@ bl dibujar_rectangulo
 
 mov x0, x20   
 movz x10, 0x0000, lsl 00 // Color Blanco
-movk x10, 0x0000, lsl 16 
-mov x11, #140 // Posición Y final
-mov x12, #490  // Posición X final
-mov x13, #101  // Posición Y inicial
+movk x10, 0x00, lsl 16 
+mov x11, #150 // Posición Y final
+mov x12, #510  // Posición X final
+mov x13, #111  // Posición Y inicial
 mov x14, #480  // Posición X inicial
 mov x5, SCREEN_WIDTH // Ancho de la pantalla	
 bl dibujar_rectangulo
 
 mov x0, x20   
-movz x10, 0x0000, lsl 00 // Color Blanco
-movk x10, 0x0000, lsl 16 
+movz x10, 0xFFFF, lsl 00 // Color Blanco
+movk x10, 0x00FF, lsl 16 
 mov x11, #129 // Posición Y final
 mov x12, #510  // Posición X final
 mov x13, #120  // Posición Y inicial
@@ -485,13 +485,14 @@ bl dibujar_rectangulo
 
 mov x0, x20   
 movz x10, 0xFFFF, lsl 00 // Color Blanco
-movk x10, 0xFFFF, lsl 16 
+movk x10, 0x00FF, lsl 16 
 mov x11, #143 // Posición Y final
 mov x12, #500  // Posición X final
 mov x13, #134  // Posición Y inicial
 mov x14, #480  // Posición X inicial
 mov x5, SCREEN_WIDTH // Ancho de la pantalla	
 bl dibujar_rectangulo
+
 
 
 //---------------------------- Arbol ---------------------------//
